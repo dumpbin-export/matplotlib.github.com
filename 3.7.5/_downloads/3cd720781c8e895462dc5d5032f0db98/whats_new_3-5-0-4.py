6658b6281348fa119fcd8f9fe2938986a9c20e07
dumpbin-export <94368856+dumpbin-export@users.noreply.github.com>
2025-11-04 12:10:55 +0800

    Delete 3.7.5 directory

--- 3.7.5/_downloads/3cd720781c8e895462dc5d5032f0db98/whats_new_3-5-0-4.py
+++
@@ -1,3 +0,0 @@
-fig = plt.figure(figsize=(3, 1))
-fig.text(0.5, 0.5, r'$\dot{a} \ddot{b} \dddot{c} \ddddot{d}$', fontsize=40,
-         horizontalalignment='center', verticalalignment='center')
--- 3.7.5/_downloads/4a78f13af6942af5ca2d97ce26757723/whats_new_3-7-0-6.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-colors = np.linspace(0, 1, 90).reshape((5, 6, 3))
-plt.pcolormesh(colors)
-plt.show()
