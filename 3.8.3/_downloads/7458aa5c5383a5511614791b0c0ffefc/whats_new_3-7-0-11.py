09d321d0ece9b481685246d62d0562a6e2dc0658
dumpbin-export <94368856+dumpbin-export@users.noreply.github.com>
2025-11-04 12:31:59 +0800

    Delete 3.8.3 directory

--- 3.8.3/_downloads/7458aa5c5383a5511614791b0c0ffefc/whats_new_3-7-0-11.py
+++
@@ -1,8 +0,0 @@
-fig, axd = plt.subplot_mosaic(
-    "AB;CD",
-    per_subplot_kw={
-        "A": {"projection": "polar"},
-        ("C", "D"): {"xscale": "log"},
-        "B": {"projection": "3d"},
-    },
-)
