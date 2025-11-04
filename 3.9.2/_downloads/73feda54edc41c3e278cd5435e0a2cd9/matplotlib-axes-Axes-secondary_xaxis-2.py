2f6cb5a5f0073456c219ca5226c514835236bd09
dumpbin-export <94368856+dumpbin-export@users.noreply.github.com>
2025-11-04 12:39:47 +0800

    Delete 3.9.2 directory

--- 3.9.2/_downloads/73feda54edc41c3e278cd5435e0a2cd9/matplotlib-axes-Axes-secondary_xaxis-2.py
+++
@@ -1,6 +0,0 @@
-fig, ax = plt.subplots()
-ax.plot(range(0, 5), range(-1, 4))
-
-# Pass 'ax.transData' as a transform to place the axis
-# relative to your data at y=0
-secax = ax.secondary_xaxis(0, transform=ax.transData)
