131a9095275b9be4c3f327780b4e0e9515b5e322
dumpbin-export <94368856+dumpbin-export@users.noreply.github.com>
2025-11-04 15:04:30 +0800

    Delete 3.10.3 directory

--- 3.10.3/_downloads/3866cf7a03a4b5b3f6c2377931b49f08/fonts-1.py
+++
@@ -1,6 +0,0 @@
-fig, ax = plt.subplots()
-ax.text(
-    .5, .5, "There are 几个汉字 in between!",
-    family=['DejaVu Sans', 'Noto Sans CJK JP', 'Noto Sans TC'],
-    ha='center'
-)
--- 3.10.3/_downloads/78415f083929c35b4cd78cb1b3f9f83f/whats_new_3-8-0-2.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-fig, ax = plt.subplots()
-ax.ecdf(np.random.randn(100))
