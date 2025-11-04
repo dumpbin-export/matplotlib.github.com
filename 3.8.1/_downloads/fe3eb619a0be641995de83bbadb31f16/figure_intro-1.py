493fe29f4c7fc1d4e90c460c9329e9048fba901d
dumpbin-export <94368856+dumpbin-export@users.noreply.github.com>
2025-11-04 12:13:40 +0800

    Delete 3.8.1 directory

--- 3.8.1/_downloads/fe3eb619a0be641995de83bbadb31f16/figure_intro-1.py
+++
@@ -1,5 +0,0 @@
-fig = plt.figure(figsize=(2, 2), facecolor='lightskyblue',
-                 layout='constrained')
-fig.suptitle('Figure')
-ax = fig.add_subplot()
-ax.set_title('Axes', loc='left', fontstyle='oblique', fontsize='medium')
