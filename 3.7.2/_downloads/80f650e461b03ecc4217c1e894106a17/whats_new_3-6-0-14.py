8437cc43079c07a95f8dba9e1d46d4a7c05624d1
dumpbin-export <94368856+dumpbin-export@users.noreply.github.com>
2025-11-04 12:08:37 +0800

    Delete 3.7.2 directory

--- 3.7.2/_downloads/80f650e461b03ecc4217c1e894106a17/whats_new_3-6-0-14.py
+++
@@ -1,4 +0,0 @@
-fig, axs = plt.subplots(3, 1)
-for i, alignment in enumerate(['left', 'center', 'right']):
-    axs[i].plot(range(10), label='test')
-    axs[i].legend(title=f'{alignment=}', alignment=alignment)
