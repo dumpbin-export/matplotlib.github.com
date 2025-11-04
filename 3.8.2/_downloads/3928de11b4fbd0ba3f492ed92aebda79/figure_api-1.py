89fb86493bf2565e2bf423b50b1f584499c499cd
dumpbin-export <94368856+dumpbin-export@users.noreply.github.com>
2025-11-04 12:14:20 +0800

    Delete 3.8.2 directory

--- 3.8.2/_downloads/3928de11b4fbd0ba3f492ed92aebda79/figure_api-1.py
+++
@@ -1,4 +0,0 @@
-fig, ax = plt.subplots(figsize=(2, 2), facecolor='lightskyblue',
-                       layout='constrained')
-fig.suptitle('Figure')
-ax.set_title('Axes', loc='left', fontstyle='oblique', fontsize='medium')
--- 3.8.2/_downloads/4afa6aaf1de4ec5296b1734d37b5d152/index-1.py
+++
@@ -1,5 +0,0 @@
-fig = plt.figure(figsize=(4, 2), facecolor='lightskyblue',
-                 layout='constrained')
-fig.suptitle('A nice Matplotlib Figure')
-ax = fig.add_subplot()
-ax.set_title('Axes', loc='left', fontstyle='oblique', fontsize='medium')
